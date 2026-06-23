9f415b874c4026621df53a464616f1a5eba29c27
bharatraghupatruni-ai <bharatraghupatruni@gmail.com>
2026-06-23 20:11:02 +0530

    feat: add eslint config file to repository

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+  }),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@next/next/no-img-element": "off",
+      "react/no-unescaped-entities": "off",
+      "react-hooks/set-state-in-effect": "off"
+    }
+  }
+];
+
+export default eslintConfig;
